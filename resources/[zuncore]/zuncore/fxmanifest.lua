6ef6fcb4cd43c6760326245b18ba26c8b353d75d
Zuntie <ZuntieDev@gmail.com>
2022-05-08 21:15:34 +0200

    Core File

---
+++ resources/[zuncore]/zuncore/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Zuntie#5340'
+description 'ZunCores Core'
+version '1.0'
+
+server_scripts {
+    'modules/updatechecker.lua',
+}
+
+client_scripts {
+
+}
