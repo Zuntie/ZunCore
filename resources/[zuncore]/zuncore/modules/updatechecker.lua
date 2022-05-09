7022c6add5c07b10227010a2dd5f2d6b7aa68d4d
Zuntie <ZuntieDev@gmail.com>
2022-05-09 20:18:41 +0200

    Small update in updatechecker.lua

--- resources/[zuncore]/zuncore/modules/updatechecker.lua
+++ resources/[zuncore]/zuncore/modules/updatechecker.lua
@@ -1,16 +1,17 @@
 if Config.CheckForUpdates then
     AddEventHandler('onResourceStart', function(resourceName)
+        local url = 'https://raw.githubusercontent.com/Zuntie/ZunCore/main/resources/%5Bzuncore%5D/zuncore/version'
         if resourceName == GetCurrentResourceName() then
-            PerformHttpRequest("https://raw.githubusercontent.com/Zuntie/ZunCore/main/resources/%5Bzuncore%5D/zuncore/modules/version", function (errorCode, resultData, resultHeaders)
+            PerformHttpRequest(url, function (errorCode, resultData, resultHeaders)
                 local version = tonumber(resultData)
                 local curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
                 local curVersion_ = tonumber(curVersion)
                 if version > curVersion_ then
-                    print("[ZunCore] Attention: There is a new version of ZunCore available!\nCurrent version: " .. curVersion_ .. "\nNew version: " .. version)
+                    print("^5[ZunCore]^0 There is a new version of ZunCore available! Current version: ^1" .. curVersion_ .. "^0 | New version: ^2" .. version)
                 elseif version == curVersion_ then
-                    print("[ZunCore] You are using the latest version of ZunCore ("..curVersion_..")!")
+                    print("^5[ZunCore]^0 You are using the latest version of ^5ZunCore ^0(^5"..curVersion_.."^0)!")
                 else
-                    print("[ZunCore] Something has gone wrong, while updating. Current version: " .. tonumber(curVersion) .. " | Github version: " .. version)
+                    print("^5[ZunCore]^0 Something has gone wrong, while updating. Current version: ^1" .. tonumber(curVersion) .. "^0 | Github version: ^2" .. version)
                 end
             end)
         end
