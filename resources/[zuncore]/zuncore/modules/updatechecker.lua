eb010cdaa6eb5c71d93c08fae075e309c7bd1153
Zuntie <ZuntieDev@gmail.com>
2022-05-09 19:03:25 +0200

    Update updatechecker.lua

--- resources/[zuncore]/zuncore/modules/updatechecker.lua
+++ resources/[zuncore]/zuncore/modules/updatechecker.lua
@@ -1,23 +1,25 @@
-if Config.checkForUpdates then
+if Config.CheckForUpdates then
     Citizen.CreateThread(
         function()
             updatePath = "/Zuntie/ZunCore"
-            resourceName = "ZunCore (" .. GetCurrentResourceName() .. ")"
+            resourceName = "ZunCore ("..GetCurrentResourceName()..")"
             PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/main/version", checkVersion, "GET")
     end)
 end
 
 function checkVersion(err, responseText, headers)
     curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
-
+    
     if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
         updateavail = true
-        print("[ZunCore] " .. resourceName .. " is outdated. Current version: " .. curVersion .. ", latest version: " .. responseText)
-    elseif tunumber(curVersion) > tonumber(responseText) then
-        updateavail = false
-        print("[ZunCore] " .. resourceName .. " git version is: "..responseText..", installed version: " .. curVersion)
+        print("\n^1----------------------------------------------------------------------------------^7")
+        print(resourceName.." is outdated, latest version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!\nupdate from https://github.com"..updatePath.."")
+        print("^1----------------------------------------------------------------------------------^7")
+    elseif tonumber(curVersion) > tonumber(responseText) then
+        print("\n^3----------------------------------------------------------------------------------^7")
+        print(resourceName.." git version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!")
+        print("^3----------------------------------------------------------------------------------^7")
     else
-        print("[ZunCore] " .. resourceName .. " is up to date. Current version: " .. curVersion)
         return
     end
 end
